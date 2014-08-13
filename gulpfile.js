799de3cffb072c1e637cd36109ca79bffd8704e9
Jorge Yau <jyau01@citymail.cuny.edu>
2014-08-13 09:46:45 -0400

    Adding build and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,26 @@
  * Description:
  * Client-side tests with modern browser console
  */
-
 'use strict';
 
+// Load dependecies
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var rimraf = require('rimraf');
 
+// Task: clean 'build'
+gulp.task('clean', function(cb) {
+  rimraf('build/', cb);
+});
+
+// Task: uglify js in 'src'
 gulp.task('uglify', function() {
   gulp.src('src/*.js')
     .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build'));
 });
+
+// Task: Run and generate 'build/'
+gulp.task('default', ['clean', 'uglify']);
